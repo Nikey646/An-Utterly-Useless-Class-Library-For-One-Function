4f1d9515d02b5208093a2a0f8403ce19fc64abb7
Nikey <Nikey646@users.noreply.github.com>
2015-05-29 22:51:18 +1000

    Project Files

---
+++ An Utterly Useless Class Library For One Function/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net;
+using CsQuery;
+
+namespace AnUtterlyUselesClassLibraryForOneFunction
+{
+    public class AnUtterlyUselessClassForOneFunction
+    {
+	    public string AnUtterlyUselessFunctionWithThirdPartyDependenciesToGetiOSGodsProfileImage(Int32 id)
+	    {
+		    using (var Client = new WebClient())
+		    {
+			    var Data = Client.DownloadString(string.Format("http://iosgods.com/user/{0}-", id));
+			    var Dom = CQ.Create(Data);
+
+			    return Dom["#profile_photo"].Attr("src");
+		    }
+	    }
+    }
+}
